89a0e97db934eea871a3166214b0ad3cbef7fbbb
pandaulait <show.hey.panda@gmail.com>
2022-05-30 22:37:44 +0900

    rails s

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :prefectures
+  root to: 'home#top'
+  get 'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
